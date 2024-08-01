109f0912bf68589738206816911ada505f6a2e77
heroyooi <heroyooi1018@gmail.com>
2024-08-01 22:21:01 +0900

    수정 완료!

--- js/ui.js
+++ js/ui.js
@@ -17,8 +17,9 @@ if ($.datepicker) {
     // changeYear: true,
     // changeMonth: true,
     dayNamesMin: ['일','월','화','수','목','금','토'],
-    monthNamesShort: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
-    showOtherMonths: true
+    monthNamesShort: ['01','02','03','04','05','06','07','08','09','10','11','12'],
+    showOtherMonths: true,
+    yearSuffix: '',
   });
 }
 
@@ -156,10 +157,20 @@ var GUI = window.GUI || (function(){
         _.find('.datepicker').datepicker({
           showOn:"button",
           buttonImage: "img/icon/calendar.svg", // type 2
-
-          // type 2에서 추가
-          beforeShow: function() {
+          onChangeMonthYear: function(year, month, inst) {
+            setTimeout(function() {
+                var dateText = (month < 10 ? '0' + month : month) + '.' + year;
+                $(".ui-datepicker-title").text(dateText);
+            }, 1);
+          },
+          beforeShow: function(input, inst) {
             $('#ui-datepicker-div').addClass('s-datepicker');
+            setTimeout(function() {
+                var month = inst.selectedMonth + 1;
+                var year = inst.selectedYear;
+                var dateText = (month < 10 ? '0' + month : month) + '.' + year;
+                $(".ui-datepicker-title").text(dateText);
+            }, 1);
           }
         });
       }
